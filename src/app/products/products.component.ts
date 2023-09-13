83a4d4350c25744158e1f5ea1521cabef9f15691
saifullahhakro <saifullah.lutufullah@purevpn.com>
2023-09-13 12:30:01 +0500

    First Commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+selector : "products", // <products></products>
+templateUrl: "./products.component.html" 
+
+})
+
+export class ProductsComponent
+{
+
+}
